1b44ffd099e01f9dca95fc86f27ef6254f312813
mjesusoj <m.jesusojedasalvador@gmail.com>
2021-10-21 10:09:19 +0200

    Upload the project

---
+++ src/modules/daybook/store/journal/getters.js
@@ -0,0 +1,11 @@
+/* export const myGetter = ( state ) => {
+    return state
+} */
+
+export const getEntriesByTerm = () => {
+
+}
+
+export const getEntryById = () => {
+    
+}
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>Hello World!</h1>
+
+    <button @click="goToDaybook" class="btn btn-primary">Primary</button>
+    <button @click="goToDaybook" class="btn btn-secondary">Secondary</button>
+    <button @click="goToDaybook" class="btn btn-success">Success</button>
+  </div>
+</template>
+
+<script>
+  // @ is an alias to /src
+  export default {
+    methods: {
+      goToDaybook() {
+        this.$router.push({ name: 'no-entry'});
+      }
+    }
+  }
+</script>
